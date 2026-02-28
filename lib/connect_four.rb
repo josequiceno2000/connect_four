477c94c46182670545750a315aef2c56c3d883a6
Jose Quiceno <josequiceno000@gmail.com>
2026-02-27 20:16:31 -0600

    Write connect_four.rb

---
+++ lib/connect_four.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'connect_four/board'
+require_relative 'connect_four/game'
+require_relative 'connect_four/plater'
+
+module ConnectFour
+  class Error < StandardError; end
+end
